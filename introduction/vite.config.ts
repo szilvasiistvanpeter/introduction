6586ab75fd4e5d864f152f11d17c1b40f9c2798a
Szilvási István Péter <szilvasipeter@protonmail.com>
2026-05-08 15:21:28 +0200

    fix: standardize code formatting in vite.config.ts

--- introduction/vite.config.ts
+++ introduction/vite.config.ts
@@ -1,9 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
   // The base path is set to 'introduction' to ensure that all assets are correctly resolved when the application is served from this subdirectory.
   base: 'introduction',
   plugins: [react()],
-})
+});
