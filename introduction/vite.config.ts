768a2e1da0589a160d347cb167a8576ab0c34afc
Szilvási István Péter <szilvasipeter@protonmail.com>
2026-05-08 14:05:23 +0200

    add CI workflow and About component with project documentation

--- introduction/vite.config.ts
+++ introduction/vite.config.ts
@@ -3,5 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  // The base path is set to 'introduction' to ensure that all assets are correctly resolved when the application is served from this subdirectory.
+  base: 'introduction',
   plugins: [react()],
 })
